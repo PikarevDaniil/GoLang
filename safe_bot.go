37a2747f2f41c4d8814ca58cc4278a308a7c0dae
DanPik <118824689+PikarevDaniil@users.noreply.github.com>
2024-06-25 16:34:35 +0300

    Create safe_bot.go

---
+++ safe_bot.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Safe struct {
+	site  string
+	login string
+	pswd  string
+}
+
+type User struct {
+	id   int64
+	name string
+	flag uint8
+	box  Safe
+}
+
+func main() {
+	the_bot()
+}
+
+func set_tools() (*sql.DB, *tgbotapi.BotAPI, tgbotapi.UpdatesChannel) {
+	// mySQL connecting
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/boxes")
+	if err != nil {
+		panic(err)
+	}
+	db.Exec("CREATE TABLE users (site VARCHAR(255), login VARCHAR(255), password VARCHAR(255))")
+	// Bot Settings
+	bot, err := tgbotapi.NewBotAPI("telegram_token")
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = false
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// Update Settings
+	u := tgbotapi.NewUpdate(0)
+	updates := bot.GetUpdatesChan(u)
+
+	return db, bot, updates
+}
+
+func the_bot() {
+	// Variables
+	users := make(map[int64]User)
+	db, bot, updates := set_tools()
+
+	// Main loop
+	for update := range updates {
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "") // Create a new Message instance
+		current_user, ok := users[update.Message.Chat.ID]
+		if !ok {
+			current_user = User{update.Message.Chat.ID, update.Message.Chat.UserName, 0, Safe{}}
+		}
+
+		request := "create table if not exists" + current_user.name + " (site varchar(255), login varchar(255), pswd varchar(255)"
+		db.Exec(request)
+		// Ignore any non-Message Updates
+		if update.Message == nil {
+			continue
+		}
+
+		// anser to non-command message
+		if !update.Message.IsCommand() {
+			switch current_user.flag {
+			case 0:
+				msg.Text = "..."
+			case 1, 2, 3:
+				current_user, msg.Text = to_write(current_user, update.Message.Text, db)
+			case 4:
+				read_data(db, update.Message.Text, bot, current_user)
+				msg.Text = "Done!"
+			case 5:
+				msg.Text = del_data(db, update.Message.Text, current_user)
+			}
+		} else {
+
+			// Handle the command
+			switch update.Message.Command() {
+			case "start":
+				msg.Text = "Hi, " + update.Message.Chat.UserName
+			case "help":
+				msg.Text = "I have /add, /find, /del commands for working with your data"
+			case "add":
+				msg.Text = "Enter site/software name..."
+				current_user.flag = 1
+			case "find":
+				msg.Text = "Enter site/software name..."
+				current_user.flag = 4
+			case "del":
+				msg.Text = "Enter site/software name..."
+				current_user.flag = 5
+			default:
+				msg.Text = "I don't know that command"
+			}
+		}
+
+		// Update the user map with the new user_flag
+		users[update.Message.Chat.ID] = current_user
+		// Send the Message
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+func to_write(user User, text string, db *sql.DB) (User, string) {
+	out := "..."
+	switch user.flag {
+	case 1:
+		user.box.site = text
+		out = "Enter login..."
+		user.flag = 2
+	case 2:
+		user.box.login = text
+		out = "Enter password..."
+		user.flag = 3
+	case 3:
+		user.box.pswd = text
+		out = "Done"
+		write_data(db, user)
+		log.Println(user.box)
+		user.flag = 0
+	}
+	return user, out
+}
+
+func write_data(db *sql.DB, user User) {
+	request := "INSERT INTO " + user.name + " (site, login, password) VALUES (?, ?, ?)"
+	db.Exec(request, user.box.site, user.box.login, user.box.pswd)
+}
+
+func read_data(db *sql.DB, site string, bot *tgbotapi.BotAPI, user User) {
+	out := "..."
+	request := "SELECT login, password FROM " + user.name + " WHERE site =?"
+	rows, err := db.Query(request, site)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer rows.Close()
+	var login string
+	var pswd string
+	for rows.Next() {
+		err := rows.Scan(&login, &pswd)
+		if err != nil {
+			log.Panic(err)
+		}
+		out = "Login: " + login + "\nPassword: " + pswd
+		msg := tgbotapi.NewMessage(user.id, out)
+		bot.Send(msg)
+	}
+}
+
+func del_data(db *sql.DB, site string, user User) string {
+	request := "DELETE FROM " + user.name + " WHERE site =?"
+	db.Exec(request, site)
+	return "Done!"
+}
